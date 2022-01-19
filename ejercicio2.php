576244e69d42725bd5c2279420f540756140940d
Miguel García <mikelrapsolo@gmail.com>
2022-01-19 14:27:16 +0100

    segundo commit

---
+++ ejercicio2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    echo "Toiriz de Arnego"."</br>"; // echo "Toririz de Arnego </br> tamen funciona"
+    echo "678772100";
+
+    ?>
+</body>
+</html>
