8eff1ca105831e1f13fbfbddcabe9cdaf5cf0ae0
Miguel García <mikelrapsolo@gmail.com>
2022-01-20 13:43:42 +0100

    quinto commit

---
+++ ejercicio5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $x= 144;
+        $y= 999;
+        $suma=$x+$y;
+        $resta=$x-$y;
+        $división=$x/$y;
+        $multiplicación=$x*$y;
+        echo "A suma é ".$suma." <br>";
+        echo "A resta é $resta <br>";
+        echo "A división é $división <br>";
+        echo "A multiplicación é $multiplicación <br>";
+    ?>
+</body>
+</html>
