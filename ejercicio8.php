cc0cee9e0bb3c2f0ca895ca4c890ce8c86e210a0
Miguel García <mikelrapsolo@gmail.com>
2022-01-21 09:33:25 +0100

    Exercicio 9

---
+++ ejercicio8.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $euros=1;
+        $pesetas="166,3860";
+        $operacion=$euros*$pesetas;
+        echo "$operacion pesetas"
+
+
+
+
+    
+        
+    ?>
+    
+</body>
+</html>
